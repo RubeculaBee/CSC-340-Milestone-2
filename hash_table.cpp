683ce8e8ab6df50b09e875c064359a2c84adccee
Robin Lane <rubeculabee@gmail.com>
2026-09-22 15:23:13 -0700

    Initial Commit

---
+++ hash_table.cpp
@@ -0,0 +1,87 @@
+/**
+ * @author Robin Lane
+ * @file hash_table.cpp
+ * @brief This is the implementation file for HashTable, using hash_table.h
+ *
+ * @date 09-22-26 - Created File
+ */
+
+ #include "hash_table.h"
+#include "hash_node.h"
+
+// TODO: Comment
+HashNode** HashTable::getTable()
+{
+	//TODO: Implement
+	return nullptr;
+}
+
+// TODO: Comment
+int HashTable::getSize()
+{
+	//TODO: Implement
+	return 0;
+}
+
+// TODO: Comment
+int HashTable::calculateHashCode(int currentKey)
+{
+	//TODO: Implement
+	return 0;
+}
+
+// TODO: Comment
+bool HashTable::isEmpty()
+{
+	//TODO: Implement
+	return false;
+}
+
+// TODO: Comment
+int HashTable::getNumberOfItems()
+{
+	//TODO: Implement
+	return 0;
+}
+
+// TODO: Comment
+bool HashTable::add(int curKey, HashNode* mynode)
+{
+	//TODO: Implement
+	return false;
+}
+
+// TODO: Comment
+bool HashTable::remove(int curKey)
+{
+	//TODO: Implement
+	return false;
+}
+
+// TODO: Comment
+void HashTable::clear()
+{
+	//TODO: Implement
+	return;
+}
+
+// TODO: Comment
+HashNode* HashTable::getItem(int curKey)
+{
+	//TODO: Implement
+	return nullptr;
+}
+
+// TODO: Comment
+bool HashTable::contains(int curKey)
+{
+	//TODO: Implement
+	return false;
+}
+
+// TODO: Comment
+void HashTable::printTable()
+{
+	//TODO: Implement
+	return;
+}
